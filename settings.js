fa2bfba1f100728bc08a406e71f4ba64e22eeeb2
malick256 <abdulrajabu0749291309@gmail.com>
2025-07-25 16:30:11 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "255697608274"
+global.ownernomer = "255749291309"
 global.ownername = "𝐷𝛥𝛮𝛮𝑌"
-global.ownernumber = '255697608274'  //owner number 2
+global.ownernumber = '255749291309'  //owner number 2
 //put your phone number above
 //let the owner as Danny or you can just put your name
 
